02550a104d7e50f33ed890de29b64aa420fb1d27
GlassyBridge114 <188959481+GlassyBridge@users.noreply.github.com>
2025-09-08 06:40:59 +0300

    Edit scene 16: Scene 1 of scene 16 complete ig
    So many crabs

--- scenes/16/script.js
+++ scenes/16/script.js
@@ -1,16 +1,15 @@
 class crab {
-    constructor(scene) {
+    constructor() {
         // Creating the crab element
         this.element = document.createElement('div');
         this.element.classList.add('crab');
         // Crab image
-        this.image = new Image();
-        this.image.src = 'images/crab-walking-no-color.gif';
+        this.element.style.backgroundImage = `url(images/crab-walking-no-color.gif)`;
         // Random color for crabs
-        this.hue = Math.floor(Math.random() * 40 - 20) + 360 % 360;
-        this.element.style.filter = `hue-rotate(${this.hue}deg) saturate(200%)`;
-
-        scene.appendChild(this.element);
+        this.hue = (Math.random() * 25) + 300;
+        this.element.style.filter = `sepia(50%) saturate(1000%) contrast(180%) hue-rotate(${this.hue}deg)`;
+        // positioning crab
+        this.element.style.left = `${1}px`;
     }
 }
 
@@ -44,39 +43,41 @@ function onPlayerStateChange(event) {
             setInterval(checkVideoTime, 500);
         }
         state.currentState = 'PLAYING';
-        console.log("Video is playing.");
     } else if (event.data == YT.PlayerState.PAUSED) {
         state.currentState = 'PAUSED';
-        console.log("Video is paused.");
     } else if (event.data == YT.PlayerState.ENDED) {
         if (hasStartedPlaying) {
             hasStartedPlaying = false;
         }
         state.currentState = 'ENDED';
-        console.log("Video has ended.");
     } else if (event.data == YT.PlayerState.BUFFERING) {
         state.currentState = 'BUFFERING';
-        console.log("Video is buffering.");
     }
 }
 
-let t44 = false;
+let t29 = false;
 function checkVideoTime(scene) {
     const currentTime = player.getCurrentTime();
-    if (currentTime >= 44 && !t44) {
-        console.log("The crabs are coming!");
-        seconds44(scene);
-        t44 = true;
+    if (currentTime >= 29 && !t29) {
+        console.log("The crabs are snapping!");
+        seconds29(true);
+        t29 = true;
     }
+}
+
+function seconds29(bool){
+    const twoThree = document.getElementById("tt1");
+    twoThree.hidden = !bool;
 }
 
 function seconds44(scene) {
-    if (state.currentState === 'PLAYING') {
+    if (state.currentState === 'PLAYING') { 
 
     }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const scene = document.getElementById('scene');
-    const crab1 = new crab(scene);
+    // const scene = document.getElementById('scene');
+    // const crab1 = new crab();
+    // scene.appendChild(crab1.element);
 });
