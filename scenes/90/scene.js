f3b6ec3cf415afe7cc205d4d155066d8595057df
Aperaine <Aperaine@users.noreply.github.com>
2025-09-13 11:16:01 +0900

    Added the exported Javascript

---
+++ scenes/90/scene.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/h3Be0Yi9H5vwPt4r/scene.splinecode');
