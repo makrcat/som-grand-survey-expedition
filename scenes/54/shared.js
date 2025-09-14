d1c99765fd8f8ba2862dece4bee84ddb950d3e54
_FN10_ <mathdudex09@gmail.com>
2025-09-14 13:07:25 -0300

    fix ghp thing again

--- scenes/54/shared.js
+++ scenes/54/shared.js
@@ -23,11 +23,12 @@ if (urlSearchParams.has("from")) {
 
 Array.from(document.getElementsByClassName("change-for-ghp")).forEach(
   (elememt) => {
-    if (elememt.tagName.toLowerCase() === "a" && window.location.href.startsWith("https://xfn10x.github.io/scene-54/")) {
-      elememt.setAttribute(
-        "href",
-        "/scene-54/scenes/54" + elememt.getAttribute("href")
-      );
+    if (
+      elememt.tagName.toLowerCase() === "a" &&
+      window.location.href.startsWith("https://xfn10x.github.io/scene-54/") &&
+      !elememt.getAttribute("href").startsWith("/scene-54")
+    ) {
+      elememt.setAttribute("href", "/scene-54" + elememt.getAttribute("href"));
 
       console.log("Changed href to: " + elememt.getAttribute("href"));
     }
