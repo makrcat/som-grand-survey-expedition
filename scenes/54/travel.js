78a8f9e773eb928cebf5fc5b7a2c5646fd00345a
_FN10_ <mathdudex09@gmail.com>
2025-09-16 19:14:21 -0300

    whoops, forgot about commiting (i forget what i did since last one)

---
+++ scenes/54/travel.js
@@ -0,0 +1,61 @@
+const first = document.getElementById("first-text");
+const second = document.getElementById("second-text");
+const third = document.getElementById("third-text");
+const fact = document.getElementById("fact");
+
+const countdowntext = document.getElementById("countdown");
+
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+const timerMax = 16;
+var timer = timerMax;
+
+function updateHref() {
+  countdowntext.innerHTML = `Redirecting in ${timer}... <a href="${urlSearchParams.get(
+    "to"
+  )}">Travel now</a>`;
+}
+
+async function showText() {
+  await delay(1000);
+  //just make this a list to be easier
+  const array = [first, second, third, fact];
+
+  array.forEach(async (element, index) => {
+    await delay(3000 * index);
+    for (let colour = 0; colour < 255; colour++) {
+      await delay(10);
+      element.style.setProperty(
+        "-webkit-text-fill-color",
+        `rgb(${colour}, ${colour}, ${colour})`
+      );
+    }
+  });
+}
+
+async function countDownToRedirect() {
+  for (let i = 0; i < timerMax; i++) {
+    await delay(1000);
+    timer--;
+    updateHref();
+  }
+  countdowntext.innerHTML = "Redirecting...";
+  //if (!window.location.href.startsWith("https://xfn10x.github.io/scene-54/")) {
+    location.href = urlSearchParams.get("to");
+  //} else {
+  //  location.href = urlSearchParams.get("to");
+  //}
+}
+
+if (!urlSearchParams.has("to")) {
+  if (!window.location.href.startsWith("https://xfn10x.github.io/scene-54/")) {
+    location.href = "/scenes/54";
+  } else {
+    location.href = "/scene-54/scenes/54";
+  }
+}
+
+updateHref();
+
+showText();
+countDownToRedirect();
