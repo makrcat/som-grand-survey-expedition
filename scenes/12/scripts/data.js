e2624b41c928c015a4caa75275d893a3e771a38c
Charmunk <isaac@jedfonner.com>
2025-09-25 12:09:13 -0400

    add scene 12 (my scene)

---
+++ scenes/12/scripts/data.js
@@ -0,0 +1,11 @@
+let gameRunning = false;
+let event = 1;
+let lastGameInput = 0;
+let activePerson = "you";
+let nextPlaces = [];
+let lastPlace = "bedroom";
+let activePlace = "bedroom";
+
+const people = {
+
+}
